f986fcf6b84b9f4697178d63b083366767fd1adc
franck-tchassi <franck.tchassi@laplateforme.io>
2024-05-26 04:48:37 +0200

    Ajout du composant Header et le contenu du fichier sass  avec le style de lien actif utilisant react-router-dom

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import kosa from '@images/kosa.png'
+import { Link, useLocation } from 'react-router-dom'
+import '@header/header.scss'
 
 const Header = () => {
+  const location = useLocation();
   return (
-    <div>
-      yyyy
+    <div className='header'>
+      <div className='logo'>
+          <img src={kosa} alt='image de kasa'/>
+      </div>
+      <nav className='navbar'>
+          <ul>
+              <Link to='/'><li className={location.pathname === '/' ? 'active' : ''}>Accueil</li></Link>
+              <Link to='apropos'><li className={location.pathname === '/apropos' ? 'active' : ''}>A Propos</li></Link>
+          </ul>
+      </nav>
     </div>
   )
 }
