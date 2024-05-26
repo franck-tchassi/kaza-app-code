245b2966d9b628d7a69afda4b0fc29c4a1211ba2
franck-tchassi <franck.tchassi@laplateforme.io>
2024-05-26 17:46:50 +0200

    Ajout du composant Banner avec affichage d'images conditionnel selon la route

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import mer from '@images/mer.jpg'
+import '@banner/banner.scss'
+import montagne from '@images/montagne.jpg'
+import {useLocation} from 'react-router-dom'
+
 
 const Banner = () => {
+  const location = useLocation();
   return (
-    <div>
+    <div className='banner'>
+      {location.pathname==='/'? 
+          <div className='mer'>
+              <img src={mer} alt='image de la mer'/>
+              <p>Chez vous, <span>partout et ailleurs</span></p>
+          </div> 
+          : 
+          " "
+      }
+
+      {location.pathname==='/apropos'? 
+          <div className='montagne'>
+               <img src={montagne} alt='image de montagne'/>
+          </div> 
+          : 
+          " "
+      }
+
       
     </div>
   )
