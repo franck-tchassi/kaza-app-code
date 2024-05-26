6f50ffba73d619cd1ae2bd9754332a2e3aca7e05
franck-tchassi <franck.tchassi@laplateforme.io>
2024-05-27 01:51:33 +0200

    ajout du composant Card avec un rendu dynamique basé sur la localisation

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import '@card/card.scss'
+import { useLocation } from 'react-router-dom'
+import logement from '../../data/logements.json'
+
 
 const Card = () => {
+  const  location = useLocation();
+  
+
   return (
-    <div>
-      
+    <div className='card-container'>
+        {location.pathname==='/' &&
+          
+          logement.map((logement)=>{
+            //console.log(logement.id)
+            return(
+            <div className='card' key={logement.id}>
+              
+                <img src={logement.cover} alt={logement.title} />
+                <h2>{logement.title}</h2>
+                
+            </div>
+            )
+          })
+
+        }
     </div>
   )
 }
