c45444563243f471e76b25c12290c59ac959caae
franck-tchassi <franck.tchassi@laplateforme.io>
2024-05-28 00:31:34 +0200

    ajout des routes pour le composant banner et les pages logement et erreur

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Header from '@header/Header';
 import Footer from '@footer/Footer';
 import Accueil from '@accueil/Accueil';
 import Apropos from '@apropos/Apropos'
+import Banner from '@banner/Banner'
+import Logement from '@logement/Logement'
+import Erreur from '@erreur/Erreur';
+
+
 
 function App() {
 
@@ -12,9 +17,12 @@ function App() {
       <Router>
             <Header />
             <main className='main-container'>
+                <Banner />
                 <Routes>
                       <Route path='/' element={<Accueil />}/>
                       <Route path='/apropos' element={<Apropos />}/>
+                      <Route path='/logement' element={<Logement />}/>
+                      <Route path='*' element={<Erreur />}/>
                 </Routes>
             </main>
             <Footer />
