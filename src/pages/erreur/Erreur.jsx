c32db5127e29236d8c0b7e9d0732540221070d3a
franck-tchassi <franck.tchassi@laplateforme.io>
2024-05-28 00:23:22 +0200

    creation de la page d'erreur

--- src/pages/erreur/Erreur.jsx
+++ src/pages/erreur/Erreur.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import '@erreur/erreur.scss'
+import { Link } from 'react-router-dom'
 
 const Erreur = () => {
   return (
-    <div>
-      
+    <div className='erreur-container'>
+      <span>404</span>
+      <p className='not-exist'>Oups! La page que vous demandez n'existe pas.</p>
+      <p className='redirection'><Link to='/'>Retourner sur la page d’accueil</Link></p>
     </div>
   )
 }
